a6dccf769a5e552ef8d1831c9301c9ed0284e5f2
Maks Kowalski <kasrow12@gmail.com>
2024-06-15 14:30:14 +0200

    kolos

--- kolos.R
+++ kolos.R
@@ -1,3 +1,4 @@
+# 1
 set.seed(123)
 dane = rexp(25, 6)
 
@@ -5,8 +6,7 @@ library(MASS)
 (est = fitdistr(dane, "exponential")$est)
 (quantile(dane, 0.9))
 
-
-
+# 2
 dane = crabs$CW
 (mean(dane))
 (var(dane))
@@ -15,6 +15,8 @@ quantile(dane, 0.8)
 
 
 wykres = hist(dane)
+(wykres$mids)
+(wykres$counts)
 
 shapiro.test(dane)$p.value
 
@@ -26,22 +28,21 @@ mian.prawy = qchisq(0.95/2, n-1)^2
 (licznik/mian.lewy)
 (licznik/mian.prawy)
 
-
+# 3
 prop.test(x=24,n=500,p=0.04,alt="greater")$p.value
 prop.test(x=24,n=500,p=0.04,alt="greater")$stat
 
 prop.test(x=24, n=500, conf.level=0.99)$conf.int
 #binom.test(x=24, n=500, conf.level=0.99)$stat
 
-
+# 4
 dane = c(22,30,22,16,10)
 prob = c(dpois(0:3, 2), ppois(3, 2, lower.tail=F))
 
 (pval = chisq.test(dane, p=prob)$p.value)
 (pval = chisq.test(dane, p=prob)$stat)
 
-
-
+# 5
 x = subset(anorexia, Treat == "CBT")$Prewt
 y = subset(anorexia, Treat == "CBT")$Postwt
 
